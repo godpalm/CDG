c9300a1a54e549dc2e77dd594a0c6d6b1c11e7b5
Chayapat Kruasukont <chayapat2320@gmail.com>
2026-02-10 16:55:38 +0700

    back to dtb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',      // เพราะ NestJS รันอยู่ที่เครื่อง Mac โดยตรง
+      port: 3306,
+      username: 'palmuser',   // ใช้ MARIADB_USER ที่คุณตั้งไว้
+      password: 'palmpassword', // ใช้ MARIADB_PASSWORD ที่คุณตั้งไว้
+      database: 'mydb',       // ใช้ MARIADB_DATABASE
+      autoLoadEntities: true, 
+      entities: [User],
+      synchronize: true,      // สั่งให้สร้างตารางอัตโนมัติ (เฉพาะช่วงพัฒนา)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
